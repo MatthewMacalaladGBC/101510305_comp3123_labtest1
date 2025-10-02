00308aeb742b75e3499ffecf7d548e4d4144f411
MatthewMacalaladGBC <matthew.macalalad@georgebrown.ca>
2025-10-02 19:37:47 -0400

    empty commit (minor spacing change)  just to note that I mixed up previous commits; added create file in the remove commit

--- q3_file_remove.js
+++ q3_file_remove.js
@@ -10,7 +10,7 @@ const logDirectory = path.join(process.cwd(), "logs");
 if (fs.existsSync(logDirectory)) {
     const files = fs.readdirSync(logDirectory);
     files.forEach((file) => {
-            console.log(`Deleting file ${file}...`);
+        console.log(`Deleting file ${file}...`);
     });
     fs.rmSync(logDirectory, { recursive: true });
     console.log("Directory and its contents deleted successfully.")
